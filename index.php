300b1b4d7882f225e27d4c386c657ec3ccbdfead
Maxim Zhurilo <fumus.27@gmail.com>
2015-10-15 16:58:18 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+require_once 'vendor/autoload.php';
+
+require_once 'vendor/fzaninotto/faker/src/autoload.php';
+
+$builder = new AdamWathan\Form\FormBuilder;
+
+// <form method="POST" action="/test">
+echo $builder->open()->action('validate.php');
+
+// <input type="text" name="email">
+echo "Enter your name : " . $builder->text('name')->required() . "<br />";
+
+// <input type="text" name="email">
+echo "enter your e-mail : " . $builder->text('email')->value('example@example.com')->required() . "<br /><br />";
+
+echo "Your gender : ";
+
+// <input type="radio" name="gender" value="male">
+echo "Male " . $builder->radio('gender', 'male')->defaultToChecked();
+// <input type="radio" name="gender" value="female">
+echo "Female " . $builder->radio('gender', 'female') . "<br />";
+
+// <button type="submit">Sign Up</button>
+echo $builder->submit('Sign Up');
+
+$faker = Faker\Factory::create();
+
+echo "<br /><br />Contact us : " . "<br /><br />";
+echo $faker->email . "<br /> Phone: " . $faker->phoneNumber . "<br />";
+echo $faker->address . "<br /><br />";
---
+++ validate.php
@@ -0,0 +1,31 @@
+<?php
+require_once 'vendor/autoload.php';
+
+use Egulias\EmailValidator\EmailValidator;
+
+$validator = new EmailValidator;
+
+if (isset($_POST['email']) && isset($_POST['name'])) {
+    $data = array();
+    $data['email'] = strtolower(trim($_POST['email']));
+    $data['name'] = ucfirst(strtolower(trim($_POST['name'])));
+    $data['gender'] = $_POST['gender'];
+
+    echo "You have entered next data : <br /><br />";
+    foreach ($data as $key => $value) {
+        echo "Your $key is : <strong>$value</strong> <br />";
+    }
+    $email = $data['email'];
+    $result = $validator->isValid($data['email']);
+
+    if ($result) {
+        echo "<strong>$email</strong>" . ' is seems to be a valid email address';
+    } else {
+        if ($validator->hasWarnings()) {
+            echo 'Warning! ' . "<strong>$email</strong>" . ' has unusual/deprecated features (result code ' . var_export($validator->getWarnings(),
+                    true) . ')';
+        } else {
+            echo "<strong>$email</strong>" . ' is not a valid email address (result code ' . $validator->getError() . ')';
+        }
+    }
+}
